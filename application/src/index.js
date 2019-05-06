7dac31c944c57b3b8aab25ef31cffc45bc1ea323
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-05-06 02:46:30 -0300

    Components/ folder with .jsx files whats provides the strucuture componentized for all pages

--- application/src/index.js
+++ application/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import Home from './app/Home';
+import Home from './app/Home/Home';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
 serviceWorker.unregister();
